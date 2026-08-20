c66ad546c9eb9bf013ccbe1c453a1a46176adc1c
ItsiRyan156 <evoryan156@gmail.com>
2026-08-20 13:58:11 +0800

    Initialize Laravel API

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Mail\ContactReceived;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string',
+        ]);
+
+        // Verify hCaptcha response
+        $captchaResponse = $request->input('captchaToken');
+        $secretKey = env('HCAPTCHA_SECRET_KEY');
+        $response = Http::asForm()->post('https://api.hcaptcha.com/siteverify', [
+            'secret' => $secretKey,
+            'response' => $captchaResponse,
+        ]);
+
+        // Check if the hCaptcha verification was successful
+        if (!$response->json('success')) {
+            return response()->json([
+                'message' => 'Captcha verification failed.'
+                ], 422);
+        }
+
+        // You can save the validated data to the database or send an email here
+        // For example, you can create a new Contact model instance and save it
+
+        $contact = Contact::create($validatedData); // Save the contact form data to the database
+
+        // Send email notification
+        Mail::to(env('CONTACT_EMAIL_ADDRESS'))->send(new ContactReceived($contact));
+
+        return response()->json(['message' => 'Contact form submitted successfully.'], 200);
+    }
+}
---
+++ resources/views/emails/contact-received.blade.php
@@ -0,0 +1,9 @@
+<h2>New Portfolio Contact</h2>
+
+<p><strong>Name:</strong> {{ $contact->name }}</p>
+<p><strong>Email:</strong> {{ $contact->email }}</p>
+<p><strong>Subject:</strong> {{ $contact->subject }}</p>
+
+<p><strong>Message:</strong></p>
+
+<p>{{ $contact->message }}</p>
